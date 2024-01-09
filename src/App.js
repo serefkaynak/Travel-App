79e8ced192d0bb6696d63656cd48012de544559e
Şeref Kaynak <serefkynk@gmail.com>
2024-01-09 20:15:01 +0300

    setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline text-red-400">
+      Hello world!
+    </h1>
+  );
+}
+
+export default App;
